7b061ede7a40f699324f7938dedacab0ca1ae2e8
memex2711 <projectmemex7@gmail.com>
2025-10-29 12:41:32 +0700

    p

--- plugins/bot/adzan.py
+++ plugins/bot/adzan.py
@@ -7,28 +7,21 @@ import string
 
 @app.on_message(filters.command("adzan", prefixes="/"))
 async def adzan_handler(client, message):
-    # Extract the location from the message (assuming user provides it after the command)
     text_split = message.text.split()
     
-    # If there's no location provided after the command, set `lok` to None
     lok = text_split[1] if len(text_split) > 1 else None
 
-    # Notify the user that the process has started
     pros = await message.reply("Processing...")
 
-    # If no location is provided, inform the user and stop further execution
     if not lok:
         return await pros.edit("Please provide a location.")
 
-    # Fetch prayer times from the MuslimSalat API
     url = f"http://muslimsalat.com/{lok}.json?key=bd099c5825cbedb9aa934e255a81a5fc"    
     req = requests.get(url)
 
-    # Check if the API request was successful
     if req.status_code != 200:
         return await pros.edit(f"Could not retrieve prayer times for {lok}. Please check the location and try again.")
 
-    # Parse the result from the API response
     result = json.loads(req.text)
     tanggal = result["items"][0]["date_for"]
     kueri = result["query"]
@@ -40,7 +33,6 @@ async def adzan_handler(client, message):
     magrip = result["items"][0]["maghrib"]
     isa = result["items"][0]["isha"]
 
-    # Format the response message
     txt = f"<b>👨‍💻 Jᴀᴅᴡᴀʟ sʜᴀʟᴀᴛ ʜᴀʀɪ ɪɴɪ:\n</b>"
     txt += f"<b>📆 ᴛᴀɴɢɢᴀʟ:</b> {tanggal}\n"
     txt += f"<b>📍 ʟᴏᴋᴀsɪ:</b> {kueri}, {negara}\n"
@@ -53,10 +45,8 @@ async def adzan_handler(client, message):
     txt += f"<blockquote><b>➥ ɪsʏᴀ:</b> {isa}\n</blockquote>"
     txt += "------------------------"
 
-    # Send the prayer times to the user
     await message.reply(txt)
 
-    # Delete the "Processing..." message
     await pros.delete()
 
 
@@ -66,5 +56,6 @@ __HELP__ = """
 
 • <b>/adzan [name country]</b> – Get info adzan by country.
 
+✧ These modules by ➪ [fr rasta](https://t.me/root404byte)
 </blockquote>
 """        
